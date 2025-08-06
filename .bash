bb06e633255d0ccf83528b608998e6735aba5657
FadhilArif <125202848+FadhilArif@users.noreply.github.com>
2025-08-06 09:13:32 +0700

    Create .bash

---
+++ .bash
@@ -0,0 +1,12 @@
+/hospital-management/
+│
+├── index.html
+├── style.css
+├── script.js
+└── pages/
+    ├── dashboard.html
+    ├── pendaftaran.html
+    ├── rawat-jalan.html
+    ├── rawat-inap.html
+    ├── gawat-darurat.html
+    └── data-pasien.html
